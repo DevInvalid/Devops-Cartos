60781ec42e7a3dbc9faac6df224b1b822539f030
DevInvalid <149808707+DevInvalid@users.noreply.github.com>
2024-11-27 19:58:35 -0300

    Update main.tf

---
+++ Projeto_Terraform_DevOps/modules/vpc/main.tf
@@ -0,0 +1,96 @@
+# Criação da VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+# Subnet Pública
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+# Subnet Privada
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+# Gateway de Internet
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MainInternetGateway"
+  }
+}
+
+# Gateway NAT para Subnet Privada
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "MainNATGateway"
+  }
+}
+
+# Elastic IP para o NAT Gateway
+resource "aws_eip" "nat" {
+domain = "vpc"
+
+}
+
+# Tabela de Roteamento Pública
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+# Associação da Tabela de Roteamento Pública com a Subnet Pública
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Tabela de Roteamento Privada
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+# Associação da Tabela de Roteamento Privada com a Subnet Privada
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
